e2901a745e4c6eb900882c51c96cd3172b591cb3
Boderio <boderiotrigan@hotmail.com>
2022-12-13 11:36:18 +0100

    cours - 1 : object keys

--- src/App.js
+++ src/App.js
@@ -42,27 +42,26 @@ class App extends Component {
    
   render() { 
     // const {titre} = this.props
+    // ["membre1","membre2","membre3","membre4"]
+    // const tab = [1,2,3,4]
+    // tab.map(iteration => { return iteration+1 })
+    // [2,3,4,5]
+    // this.state.league.membre1.age
+    // this.state.league.iteration.age
+    // this.state.league[iteration].age
+    const list = Object.keys(this.state.league).map(iteration => {
+      return (
+        <Membre key={iteration} age={this.state.league[iteration].age} nom={this.state.league[iteration].nom} />
+      )
+    })
     return (
         <Fragment>
           <div className='App'>
             <h1>React App</h1>
             <h4>{this.props.titre}</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae illum harum pariatur quia deserunt ad accusantium blanditiis quaerat amet, quae reiciendis eveniet tenetur totam accusamus, minima nulla. Adipisci aspernatur iusto necessitatibus aliquid. Eum rem recusandae aliquid rerum labore distinctio omnis tempore aspernatur fugiat corrupti voluptas, nisi quasi quae. Excepturi iste nesciunt sit distinctio eveniet incidunt atque commodi minus dolor fugit!</p>
-            {this.state.league.membre1.nom}
-            <Membre 
-              nom={this.state.league.membre1.nom} 
-              age={this.state.league.membre1.age} 
-            />
-            <Membre 
-              nom={this.state.league.membre2.nom} 
-              age={this.state.league.membre2.age} 
-            />
-            <Membre 
-              nom="Napoléon"
-              age={4}
-            >
-              Je suis un empereur
-            </Membre>
+            {list}
+          
             <Button 
               plus = {this.state.plus}
               vieillir = {() => this.handleClick(this.state.plus)}
